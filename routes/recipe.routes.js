874b01f4220568142b9237079397ad5d5a309765
andersontoma92WDPTOCT2022 <115597537+andersontoma92WDPTOCT2022@users.noreply.github.com>
2022-11-30 15:47:12 -0300

    iteration 3, insert many

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -5,6 +5,8 @@ const recipeRoute = express.Router();
 //
 //rotas
 //
+//Iteration 2 - Create a recipe
+//
 recipeRoute.post('/create-recipe', async (req, res) => {
   try {
     const form = req.body;
@@ -18,8 +20,26 @@ recipeRoute.post('/create-recipe', async (req, res) => {
     return res.status(500).json(error.errors);
   }
 });
-
 //
+//Iteration 3 - Insert multiple recipes
+//
+recipeRoute.post('/create-many', async (req, res) => {
+  try {
+    const form = req.body;
+
+    //quer criar um documento dentro da sua collection -> .create()
+    const newRecipe = await RecipeModel.create(form);
+    //
+    form.forEach((receita) => {
+      console.log(receita.title, '<-- create many, titulo');
+    });
+    //
+    return res.status(201).json(newRecipe);
+  } catch (error) {
+    console.log(error.errors);
+    return res.status(500).json(error.errors);
+  }
+});
 //
 //
 export default recipeRoute;
