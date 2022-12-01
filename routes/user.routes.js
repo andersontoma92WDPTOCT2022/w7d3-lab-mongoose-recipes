959d410367e9865091fed05e75dc8cf4b1cf39ce
andersontoma92WDPTOCT2022 <115597537+andersontoma92WDPTOCT2022@users.noreply.github.com>
2022-12-01 14:54:39 -0300

    2.2  rota GET /read/:id

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -22,4 +22,29 @@ userRoute.post('/create', async (req, res) => {
     return res.status(500).json(error.errors);
   }
 });
+//
+// user//read/:userId
+// 2.2 Crie a rota GET /read/:userId
+//
+userRoute.get('/read/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id, '-id------- dentro do read/ID----------');
+    // const user = await UserModel.find({_id: id})
+    const user = await UserModel.findById(id).populate('recipes');
+    // usuário existe?
+    if (!user) {
+      return res.status(400).json({ msg: ' Usuário não encontrado!' });
+    }
+
+    return res.status(200).json(user);
+    //
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.errors);
+  }
+});
+//
+//  exportar p/ index.js!!
+//
 export default userRoute;
